5202b524af3fbcb7aa09b3405de568f8e3e3ca0a
ТвоёмяаGitHub <@.com>
2025-10-26 07:16:36 +0500

    feat(algorithm): implement Kruskal's algorithm (KruskalMST)

--- src/main/java/algorithms/KruskalMST.java
+++ src/main/java/algorithms/KruskalMST.java
@@ -1,4 +1,94 @@
 package algorithms;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class KruskalMST {
+
+    private static final double FLOATING_POINT_EPSILON = 1.0E-12;
+    private double weight;
+    private Queue<Edge> mst = new LinkedList<>();
+
+    public KruskalMST(EdgeWeightedGraph G) {
+        Edge[] edges = new Edge[G.E()];
+        int t = 0;
+        for (Edge e : G.edges()) {
+            edges[t++] = e;
+        }
+
+        Arrays.sort(edges);
+
+        UF uf = new UF(G.V());
+
+        for (Edge e : edges) {
+            int v = e.either();
+            int w = e.other(v);
+
+            if (uf.find(v) != uf.find(w)) {
+                uf.union(v, w);
+                mst.add(e);
+                weight += e.weight();
+            }
+        }
+
+        assert check(G);
+    }
+
+    public Iterable<Edge> edges() {
+        return mst;
+    }
+
+    public double weight() {
+        return weight;
+    }
+
+    private boolean check(EdgeWeightedGraph G) {
+        double total = 0.0;
+        for (Edge e : edges()) {
+            total += e.weight();
+        }
+
+        if (Math.abs(total - weight()) > FLOATING_POINT_EPSILON) {
+            System.err.printf("Weight of edges does not equal weight(): %f vs. %f\n", total, weight());
+            return false;
+        }
+
+        UF uf = new UF(G.V());
+        for (Edge e : edges()) {
+            int v = e.either(), w = e.other(v);
+            if (uf.find(v) == uf.find(w)) {
+                System.err.println("Not a forest");
+                return false;
+            }
+            uf.union(v, w);
+        }
+
+        for (Edge e : G.edges()) {
+            int v = e.either(), w = e.other(v);
+            if (uf.find(v) != uf.find(w)) {
+                System.err.println("Not a spanning forest");
+                return false;
+            }
+        }
+
+        for (Edge e : edges()) {
+            uf = new UF(G.V());
+            for (Edge f : edges()) {
+                int x = f.either(), y = f.other(x);
+                if (f != e) uf.union(x, y);
+            }
+            for (Edge f : G.edges()) {
+                int x = f.either(), y = f.other(x);
+                if (uf.find(x) != uf.find(y)) {
+                    if (f.weight() < e.weight()) {
+                        System.err.println("Edge " + f + " violates cut optimality conditions");
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
 }
