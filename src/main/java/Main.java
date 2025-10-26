9bd7aa6d76f06ff4ae35467ce675b6a6ac5f86b1
ТвоёмяаGitHub <@.com>
2025-10-26 08:59:51 +0500

    feat(Main): implement input data reading from JSON (assign_3_input.json)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,56 @@
-package PACKAGE_NAME;
+
+import algorithms.EdgeWeightedGraph;
+import algorithms.PrimMST;
+import algorithms.KruskalMST;
+import utils.GraphLoader;
+import metrics.Metrics;
+import org.json.JSONObject;
+
+
+import java.io.FileWriter;
+import java.io.IOException;
+
 
 public class Main {
+
+    public static void main(String[] args) {
+        Metrics.startTimer();
+
+        String inputFilePath = "src/main/resources/assign_3_input.json";
+        String outputFilePath = "src/main/resources/output.json";
+
+        try {
+            EdgeWeightedGraph graph = GraphLoader.loadGraphFromJson(inputFilePath);
+
+            PrimMST primMST = new PrimMST(graph);
+            System.out.println("Prim's Algorithm:");
+            System.out.println("Total MST cost: " + primMST.weight());
+            Metrics.printMetrics();
+
+            KruskalMST kruskalMST = new KruskalMST(graph);
+            System.out.println("Kruskal's Algorithm:");
+            System.out.println("Total MST cost: " + kruskalMST.weight());
+            Metrics.printMetrics();
+
+            JSONObject result = new JSONObject();
+            result.put("primMSTCost", primMST.weight());
+            result.put("kruskalMSTCost", kruskalMST.weight());
+            result.put("executionTime", Metrics.getExecutionTime());
+            result.put("primComparisons", Metrics.getComparisonCount());
+            result.put("kruskalComparisons", Metrics.getComparisonCount());
+            result.put("primUnions", Metrics.getUnionCount());
+            result.put("kruskalUnions", Metrics.getUnionCount());
+
+            FileWriter writer = new FileWriter(outputFilePath);
+            writer.write(result.toString(4));
+            writer.close();
+
+        } catch (IOException e) {
+            System.err.println("Error reading input data: " + e.getMessage());
+        }
+
+        Metrics.stopTimer();
+    }
 }
+
+
--- src/main/java/utils/GraphLoader.java
+++ src/main/java/utils/GraphLoader.java
@@ -1,4 +1,44 @@
 package utils;
 
+import algorithms.Edge;
+import algorithms.EdgeWeightedGraph;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class GraphLoader {
+
+    public static EdgeWeightedGraph loadGraphFromJson(String filename) throws IOException {
+        FileReader reader = new FileReader(filename);
+        StringBuilder jsonBuilder = new StringBuilder();
+        int c;
+        while ((c = reader.read()) != -1) {
+            jsonBuilder.append((char) c);
+        }
+
+        JSONObject json = new JSONObject(jsonBuilder.toString());
+        JSONArray edgesArray = json.getJSONArray("edges");
+        List<Edge> edges = new ArrayList<>();
+
+        for (int i = 0; i < edgesArray.length(); i++) {
+            JSONObject edgeObj = edgesArray.getJSONObject(i);
+            int v = edgeObj.getInt("from");
+            int w = edgeObj.getInt("to");
+            double weight = edgeObj.getDouble("weight");
+            edges.add(new Edge(v, w, weight));
+        }
+
+        int vertices = json.getInt("vertices");
+        EdgeWeightedGraph graph = new EdgeWeightedGraph(vertices);
+        for (Edge e : edges) {
+            graph.addEdge(e);
+        }
+
+        return graph;
+    }
 }
+
